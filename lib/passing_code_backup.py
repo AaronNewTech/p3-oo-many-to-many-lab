803ad1e18334f22748657fd6d8f7f9349f564d2e
Aaron <arnhsmith@gmail.com>
2023-07-31 09:14:04 -0400

    passed all tests

--- lib/passing_code_backup.py
+++ lib/passing_code_backup.py
@@ -1,27 +1,35 @@
 class Author:
+
+    all = []
+
     def __init__(self, name):
         self.name = name
+        Author.all.append(self)
 
     def contracts(self):
         return [contract for contract in Contract.all if contract.author == self]
-    
+
     def books(self):
         return [contract.book for contract in Contract.all if contract.author == self]
-    
+
     def sign_contract(author, book, date, royalties):
         contract = Contract(author, book, date, royalties)
         return contract
-    
+
     def total_royalties(self):
         return sum(contract.royalties for contract in Contract.all if contract.author == self)
 
+
 class Book:
+
+    all = []
+
     def __init__(self, title):
         self.title = title
-    
+
     def contracts(self):
         return [contract for contract in Contract.all if contract.book == self]
-    
+
     def authors(self):
         return [contract.author for contract in Contract.all if contract.book == self]
 
@@ -31,26 +39,24 @@ class Contract:
     all = []
 
     def __init__(self, author, book, date, royalties):
-        if isinstance(author, Author):
-            self.author = author
-        else:
+
+        if not isinstance(author, Author):
             raise Exception("Invalid author: %s" % author)
-        
-        if isinstance(book, Book):
-            self.book = book
-        else:
+
+        if not isinstance(book, Book):
             raise Exception("Invalid book: %s" % book)
-        
-        if isinstance(date, str):
-            self.date = date
-        else:
-            raise Exception("Invalid date: %s " % date)
-        
-        if isinstance(royalties, int):
-            self.royalties = royalties
-        else:
-            raise Exception("Invalid bookmark: %s " % royalties)
-        
+
+        if not isinstance(date, str):
+            raise Exception("Invalid date: %s" % date)
+
+        if not isinstance(royalties, int) or royalties < 0:
+            raise Exception("Invalid royalties: %s" % royalties)
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
         Contract.all.append(self)
 
     @classmethod
